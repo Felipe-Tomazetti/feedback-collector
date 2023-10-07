ded0f2b1bb6cff19b3aeed6194c42e99ef1065f6
FelipeTomazetti <felipe.pistori@gmail.com>
2023-10-07 15:05:59 -0300

    login flow created

---
+++ README.md
@@ -0,0 +1,3 @@
+## A project that can be used to send emails to client to collect feedback.
+
+## Technology used: Node, React, Express, Mongoose, MongoDB 
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+//import express from 'express'; Node now have support for ES6+ sintax, adding a "type": "module" in package.json
 const express = require('express');
-//import express from 'express'; Node doesnt have support for ES2015 sintax
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport')
+
+const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-//create route handler and associate it
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+//another way of doing the same thinks as the line above
+//require('./routes/authRoutes')app();
 
 const PORT = process.env.PORT || 3000;
 
